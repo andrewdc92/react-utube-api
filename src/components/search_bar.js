f40bc08acc4f87f2bdd6685472124b942a2d3ff7
andrewdc92 <andrew.cordivari@gmail.com>
2016-12-09 15:35:44 -0800

    adds onchange + setState, input still works

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Starting Value'};
+  }
+
+  render() {
+    return (
+    <div>
+      <input
+       value={this.state.term}
+       onChange={event => this.setState({ term: event.target.value })} />
+    </div>
+    // sets the state to the empty string defined in term above
+    // when the onChange handler runs, the value of the input has not actually changed
+    // the event handler updates this.state.term with the new value, finally component finishes
+    // rendering. SO-typing in input doesn't actually update view, just triggers the event
+
+    );
+  }
+}
+
+
 
 export default SearchBar;
