60d40b4d1ecad0146fe6bb05aa8ac700b6804a16
andrewdc92 <andrew.cordivari@gmail.com>
2016-12-10 12:23:13 -0800

    videos update on click, callbacks between parent and child are connected

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCetO-Veu4hILuRuvB64uyFch0BgHdzRvA';
 
 
@@ -11,24 +12,33 @@ const API_KEY = 'AIzaSyCetO-Veu4hILuRuvB64uyFch0BgHdzRvA';
     constructor(props) {
       super(props);
 
-      this.state = { videos: [] };
+      this.state = {
+        videos: [],
+        selectedVideo: null
+      };
 
       YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({ videos });
+        this.setState({
+           videos: videos,
+           selectedVideo: videos[0]
+          });
         // calling this.setState is a callback
         // in es6, {videos} means key:videos, value: videos, just shorthand
       });
     }
     render() {
     return (
-      <div>
-         <SearchBar />
-       <VideoList videos = {this.state.videos}/>
-     </div>
+    <div>
+      <SearchBar />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+         videos = {this.state.videos}/>
+    </div>
     );
    }
   }
-  // passing prop videos to video list
+  // passing prop videos and onVideoSelect to video list
 
 
     ReactDOM.render(<App />, document.querySelector('.container'));
