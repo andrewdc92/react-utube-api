b99a34d893484fb2d43520d705d315a0b4da0b59
andrewdc92 <andrew.cordivari@gmail.com>
2016-12-09 16:32:37 -0800

    youtube search does return the video objects

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCetO-Veu4hILuRuvB64uyFch0BgHdzRvA';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-//
-// import App from './components/app';
-// import reducers from './reducers';
-//
-// const createStoreWithMiddleware = applyMiddleware()(createStore);
-//
-// ReactDOM.render(
-//   <Provider store={createStoreWithMiddleware(reducers)}>
-//     <App />
-//   </Provider>
-//   , document.querySelector('.container'));
+
 
   // Create a new component that produces HTML and renders it in DOM
-  const App = () => {
+  class App extends Component {
+    constuctor(props) {
+      super(props);
+
+      this.state = { videos: [] };
+
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });
+      });
+    }
+    render() {
     return (
       <div>
          <SearchBar />
      </div>
     );
+   }
   }
 
 
